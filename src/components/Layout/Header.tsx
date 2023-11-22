2c11a077cf2e80a1278a358aee403f1bd9c02278
Nguyen Hoang Tuan <nguyen_hoangtuan@ca-adv.co.jp>
2023-11-22 15:28:32 +0700

    Update source struct

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import IconComponent from "@/components/Icons";
 import Menu from "./Menu";
 import Logo from "../Logo";
-import CollectionSearch from "./Search";
+import CollectionSearch from "../Search";
 import { ROUTER } from "@/configs/router";
 import { getCollections } from "@/lib/saleor/graphql/fetch/getCollections";
 
